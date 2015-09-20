3a243915c88c0ec02dc4f34f23c1fb88c05adc0a
jetsly <jetsly@live.cn>
2015-09-20 21:15:52 +0800

    ok

--- src/app/bootstrap.tsx
+++ src/app/bootstrap.tsx
@@ -1,6 +1,8 @@
 /// <reference path="../typings/all.d.ts" />
 import * as React from "react"
-import App from './app'
+//import App from './app'
+let App = require( "./app.tsx").App;
+
 React.render(
     <App/>,
     document.body
