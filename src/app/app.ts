da059d8bd9f17599e4402dc59b062d65d89f5aac
jetsly <jetsly@live.cn>
2015-09-20 16:48:22 +0800

    a

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,10 @@
-export class App {
-	
+/// <reference path="../typings/all.d.ts" />
+import * as React from "react";
+
+export class App extends React.Component<any, any> {
+	 render(){
+		return (
+            <div></div>
+        );
+	 }
 }
