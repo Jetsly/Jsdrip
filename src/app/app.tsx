26929c6e57a2b255a99cfbe083d0466395a67085
jetsly <jetsly@live.cn>
2015-09-20 20:48:38 +0800

    Smart tips ok

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,7 @@
 /// <reference path="../typings/all.d.ts" />
 import * as React from "react"
 
-export class App extends React.Component<any, any> {
+export default class App extends React.Component<any, any> {
 	 render(){
 		return (
             <div>dsa</div>
