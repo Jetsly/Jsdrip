87035398b2a44530fbe3b7e3eea2e57f23ce9646
Jetsly <Jetsly@live.cn>
2015-12-16 22:33:10 +0800

    aa

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -5,10 +5,10 @@ import {
  * App Component
  * our top level component that holds all of our components
  */
-import {App} from './app';
+import {App } from './app'; 
 
 /*
  * Bootstrap our Angular app with a top level component `App` and inject
  * our Services and Providers into Angular's dependency injection
- */
+ */ 
 bootstrap(App);
